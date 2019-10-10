9365fb24d1603a055cc473cc7431a84c25a773d2
davemus <daviddrro@gmail.com>
2019-10-10 09:19:37 +0300

    Refactor Percolation

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -12,100 +12,138 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  **/
 
 public class Percolation {
-    private static final int TOP_SITE_NUMBER = 0;
-    private final int botSiteNumber;
-    private final WeightedQuickUnionUF weightedQuickUnionUF;
-    private final int side;
-    private boolean[][] openSites;
-    private int openSitesQuantity;
+	private static final int TOP_SITE_NUMBER = 0;
+	private final int botSiteNumber;
+	private final WeightedQuickUnionUF weightedQuickUnionUF;
+	private final int side;
+	private boolean[][] openSites;
+	private int openSitesQuantity;
 
-    public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        int sites = n * n + 2; // 2 sites for virtual top and bot
-        this.side = n;
-        this.weightedQuickUnionUF = new WeightedQuickUnionUF(sites);
-        this.openSites = new boolean[n][n];
-        this.botSiteNumber = sites - 1;
-        this.openSitesQuantity = 0;
-    }
-
-    private void connectNeighboursIfOpen(int row, int col) {
-        if (row == 1) {
-            this.weightedQuickUnionUF.union(TOP_SITE_NUMBER, this.toWeightUnionNum(row, col));
-        }
-	int[][] neighbourCells = {{row+1, col}, {row-1, col}, {row, col+1}, {row, col-1}};
-	for (int[] cell: neighbourCells) {
-		this.connectIfNeighbourExistsAndIsOpen(row, col, cell[0], cell[1]);
+	public Percolation(int n) {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		int sites = n * n + 2; // 2 sites for virtual top and bot
+		this.side = n;
+		this.weightedQuickUnionUF = new WeightedQuickUnionUF(sites);
+		this.openSites = new boolean[n][n];
+		this.botSiteNumber = sites - 1;
+		this.openSitesQuantity = 0;
 	}
-        if (row == this.side) {
-            this.weightedQuickUnionUF.union(this.botSiteNumber, this.toWeightUnionNum(row, col));
-        }
-    }
 
-    private int toWeightUnionNum(int row, int col) {
-        return (row - 1) * this.side + col;
-    }
+	private class Cell {
+		public final int col;
+		public final int row;
 
-    private void connectIfNeighbourExistsAndIsOpen(int row, int col, int
-            neighbourRow, int neighbourCol) {
-        if (this.isValidRowColNum(neighbourRow) && this.isValidRowColNum(neighbourCol)) {
-            if (this.openSites[neighbourRow - 1][neighbourCol - 1]) {
-                this.weightedQuickUnionUF.union(
-                        this.toWeightUnionNum(row, col),
-                        this.toWeightUnionNum(neighbourRow, neighbourCol)
-                );
-            }
-        }
-    }
+		Cell(int row, int col) {
+			this.col = col;
+			this.row = row;
+		}
 
-    private boolean isValidRowColNum(int tested) {
-        return 1 <= tested && tested <= this.side;
-    }
+		Cell[] neighbours() {
+			Cell[] cells = {
+				new Cell(this.row + 1, this.col),
+				new Cell(this.row - 1, this.col),
+				new Cell(this.row, this.col + 1),
+				new Cell(this.row, this.col - 1),
+			};
+			return cells;
+		}
 
-    public void open(int row, int col) {
-        if (!this.isValidRowColNum(row) || !this.isValidRowColNum(col)) {
-            throw new IllegalArgumentException();
-        }
-        if (!this.isOpen(row, col)) {
-            this.openSites[row - 1][col - 1] = true;
-            this.connectNeighboursIfOpen(row, col);
-            this.openSitesQuantity += 1;
-        }
-    }
+		private int toWeightUnionNum() {
+			return (this.row - 1) * side + this.col;
+		}
 
-    public boolean isOpen(int row, int col) {
-        if (!this.isValidRowColNum(row) || !this.isValidRowColNum(col)) {
-            throw new IllegalArgumentException();
-        }
-        return this.openSites[row - 1][col - 1];
-    }
+		private boolean isValid() {
+			return isValidRowColNum(this.row) && isValidRowColNum(this.col);
+		}
 
-    public boolean isFull(int row, int col) {
-        if (!this.isValidRowColNum(row) || !this.isValidRowColNum(col)) {
-            throw new IllegalArgumentException();
-        }
-        return this.openSites[row - 1][col - 1] &&
-                this.weightedQuickUnionUF
-                        .connected(TOP_SITE_NUMBER, this.toWeightUnionNum(row, col));
-    }
+		private void open() {
+			if (this.isOpen()) {
+				throw new IllegalArgumentException();
+			}
+			openSites[this.row - 1][this.col - 1] = true;
+			this.connectNeighboursIfOpen();
+			openSitesQuantity += 1;
+		}
 
-    public int numberOfOpenSites() {
-        return this.openSitesQuantity;
-    }
+		private boolean isOpen() {
+			return openSites[this.row - 1][this.col - 1];
+		}
 
-    public boolean percolates() {
-        return this.weightedQuickUnionUF.connected(TOP_SITE_NUMBER, this.botSiteNumber);
-    }
+		private void connectNeighboursIfOpen() {
+			if (this.row == 1) {
+				weightedQuickUnionUF.union(TOP_SITE_NUMBER, this.toWeightUnionNum());
+			}
+			for (Cell neighbour: this.neighbours()) {
+				this.connectIfNeighbourExistsAndIsOpen(neighbour);
+			}
+			if (this.row == side) {
+				weightedQuickUnionUF.union(botSiteNumber, this.toWeightUnionNum());
+			}
+		}
 
-    public static void main(String[] args) {
-	Percolation percolation = new Percolation(3);
-	int[][] pointsToOpen = {{1, 1}, {2, 1}, {3, 1}};
-	for (int[] point: pointsToOpen) {
-		assert !percolation.percolates();
-		percolation.open(point[0], point[1]);
+		private void connectIfNeighbourExistsAndIsOpen(Cell neighbour) {
+			if (neighbour.isValid() && neighbour.isOpen()) {
+				weightedQuickUnionUF.union(
+				this.toWeightUnionNum(), neighbour.toWeightUnionNum());
+			}
+		}
+
+		private boolean isConnectedToTop() {
+			return weightedQuickUnionUF.connected(TOP_SITE_NUMBER, this.toWeightUnionNum());
+		}
 	}
-	assert percolation.percolates();
-    }
+
+	private boolean isValidRowColNum(int tested) {
+		return 1 <= tested && tested <= this.side;
+	}
+
+	public void open(int row, int col) {
+		Cell cell = new Cell(row, col);
+		if (!cell.isValid()) {
+			throw new IllegalArgumentException();
+		}
+		if (!cell.isOpen()) {
+			cell.open();
+		}
+	}
+
+	public boolean isOpen(int row, int col) {
+		Cell cell = new Cell(row, col);
+		if (!cell.isValid()) {
+			throw new IllegalArgumentException();
+		}
+		return cell.isOpen();
+	}
+
+	public boolean isFull(int row, int col) {
+		Cell cell = new Cell(row, col);
+		if (!cell.isValid()) {
+			throw new IllegalArgumentException();
+		}
+		return cell.isOpen() && cell.isConnectedToTop();
+	}
+
+	public int numberOfOpenSites() {
+		return this.openSitesQuantity;
+	}
+
+	public boolean percolates() {
+		return this.weightedQuickUnionUF.connected(TOP_SITE_NUMBER, this.botSiteNumber);
+	}
+
+	public static void main(String[] args) {
+		Percolation percolation = new Percolation(3);
+		int[][] pointsToOpen = {
+			{1,1},
+			{2,1},
+			{3,1}
+		};
+		for (int[] point: pointsToOpen) {
+			assert ! percolation.percolates();
+			percolation.open(point[0], point[1]);
+		}
+		assert percolation.percolates();
+	}
 }
